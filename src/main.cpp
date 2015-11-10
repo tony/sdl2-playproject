6812d873de76a5a4dcbdb995b0cacbc880f6a690
Tony Narlock <tony@git-pull.com>
2015-11-10 11:52:00 -0600

    add the rest

---
+++ src/main.cpp
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <SDL.h>
+#include <SDL_image.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+bool init();
+bool loadMedia();
+void close();
+SDL_Surface* loadSurface();
+
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* gHelloWorld = NULL;
+
+
+bool init() {
+    bool success = true;
+    if (SDL_Init( SDL_INIT_VIDEO ) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        success = false;
+    } else {
+        gWindow = SDL_CreateWindow("SDL Tutorial",
+                SDL_WINDOWPOS_UNDEFINED, 
+                SDL_WINDOWPOS_UNDEFINED, 
+                SCREEN_WIDTH,
+                SCREEN_HEIGHT,
+                SDL_WINDOW_SHOWN
+                );
+        if (gWindow == NULL) {
+            printf("Window could not be created!  SDL_Error: %s\n", SDL_GetError() );
+            success = false;
+        } else {
+            gScreenSurface = SDL_GetWindowSurface( gWindow );
+            SDL_FillRect( gScreenSurface, NULL, SDL_MapRGB( gScreenSurface->format, 0xFF, 0xFF, 0xFF ) );
+            SDL_UpdateWindowSurface(gWindow);
+            SDL_Delay(2000);
+        }
+    }
+
+    return success;
+}
+
+SDL_Surface* loadSurface( const char* path )
+{
+    //The final optimized image
+    SDL_Surface* optimizedSurface = NULL;
+
+    //Load image at specified path
+    SDL_Surface* loadedSurface = IMG_Load( path );
+    if( loadedSurface == NULL )
+    {
+        printf( "Unable to load image %s! SDL_image Error: %s\n", path, IMG_GetError() );
+    }
+    else
+    {
+        //Convert surface to screen format
+        optimizedSurface = SDL_ConvertSurface( loadedSurface, gScreenSurface->format, 0 );
+        if( optimizedSurface == NULL )
+        {
+            printf( "Unable to optimize image %s! SDL Error: %s\n", path, SDL_GetError() );
+        }
+
+        //Get rid of old loaded surface
+        SDL_FreeSurface( loadedSurface );
+    }
+
+    return optimizedSurface;
+}
+
+bool loadMedia() {
+    bool success = true;
+    gHelloWorld = loadSurface("resources/continents.png");
+
+    if (gHelloWorld == NULL) {
+        printf("Unable to load image %s! SDL Error: %s\n", "resources/continents.png", SDL_GetError());
+        success = false;
+    }
+    return success;
+}
+
+void close() {
+    SDL_FreeSurface(gHelloWorld);
+    gHelloWorld = NULL;
+
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+
+    SDL_Quit();
+}
+
+int main() {
+
+    //Start up SDL and create window
+    if( !init() ) {
+        printf( "Failed to initialize!\n" );
+    } else {
+        //Load media
+        if( !loadMedia() ) {
+            printf( "Failed to load media!\n" );
+        } else {
+            //Apply the image
+            SDL_BlitSurface( gHelloWorld, NULL, gScreenSurface, NULL );
+            // Update the surface
+            SDL_UpdateWindowSurface(gWindow);
+
+            SDL_Delay(2000);
+        }
+
+    }
+    close();
+    return 0;
+}
