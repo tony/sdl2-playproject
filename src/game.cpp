62aca945dc50bf940d80c4a9db54d25a172cda99
Tony Narlock <tony@git-pull.com>
2016-03-14 12:04:26 -0500

    wrap in try/catch loops

--- src/game.cpp
+++ src/game.cpp
@@ -5,15 +5,13 @@ extern const double SCREEN_WIDTH = 630;
 extern const double SCREEN_HEIGHT = 480;
 
 extern const SDL_Rect BOTTOM_VIEWPORT_RECT = {
-  0, int(SCREEN_HEIGHT * .9), int(SCREEN_WIDTH), int(SCREEN_HEIGHT * .1)};
+    0, int(SCREEN_HEIGHT * .9), int(SCREEN_WIDTH), int(SCREEN_HEIGHT * .1)};
 
 extern const SDL_Rect MAIN_VIEWPORT_RECT = {0, 0, int(SCREEN_WIDTH),
-  int(SCREEN_HEIGHT * .9)};
-
-TTF_Font* font = NULL;
+                                            int(SCREEN_HEIGHT * .9)};
 
 bool game_load_textures(std::shared_ptr<SDL_Texture>& bgTexture,
-    SDL_Renderer* renderer) {
+                        SDL_Renderer* renderer) {
   bool success = true;
 
   bgTexture = texture_load("resources/tiles_12.png", renderer);
@@ -29,41 +27,48 @@ Game::Game(void) {
   imgFlags = IMG_INIT_PNG;
   bgTexture = nullptr;
 
-  SDL2pp::SDL sdl(SDL_INIT_VIDEO);
+  try {
+    SDL2pp::SDL sdl(SDL_INIT_VIDEO);
 
-  SDL2pp::Window window("sdl2-playproject", SDL_WINDOWPOS_CENTERED,
-      SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT,
-      SDL_WINDOW_RESIZABLE);
+    SDL2pp::Window window("sdl2-playproject", SDL_WINDOWPOS_CENTERED,
+                          SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT,
+                          SDL_WINDOW_RESIZABLE);
 
-  SDL2pp::Renderer renderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL2pp::Renderer renderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-  if (!(IMG_Init(imgFlags) & imgFlags)) {
-    fatal("SDL_image could not initialize! SDL_image Error: %s\n",
-        IMG_GetError());
+    if (!(IMG_Init(imgFlags) & imgFlags)) {
+      fatal("SDL_image could not initialize! SDL_image Error: %s\n",
+            IMG_GetError());
+    }
+
+    hero = new Hero(renderer.Get());
+
+    if (!game_load_textures(bgTexture, renderer.Get())) {
+      fatal("Failed to load media!\n");
+    } else if (!hero->load_textures()) {
+      fatal("Failed to load hero media!\n");
+    } else if (TTF_Init() == -1) {
+      fatal("Font engine failed to initialize.\n");
+    }
+
+    // load fonts
+    SDL2pp::Font font(
+        get_full_path("resources/fonts/TerminusTTF-Bold-4.39.ttf"), 36);
+
+    gamepanel = new GamePanel(hero, renderer.Get(), font.Get());
+    renderer.SetDrawColor(0xFF, 0xFF, 0xFF, 0xFF);
+  } catch (SDL2pp::Exception& e) {
+    // Exception stores SDL_GetError() result and name of function which failed
+    std::cerr << "Error in: " << e.GetSDLFunction() << std::endl;
+    std::cerr << "  Reason: " << e.GetSDLError() << std::endl;
+  } catch (std::exception& e) {
+    // This also works (e.g. "SDL_Init failed: No available video device")
+    std::cerr << e.what() << std::endl;
   }
-
-  hero = new Hero(renderer.Get());
-
-  if (!game_load_textures(bgTexture, renderer.Get())) {
-    fatal("Failed to load media!\n");
-  } else if (!hero->load_textures()) {
-    fatal("Failed to load hero media!\n");
-  } else if (TTF_Init() == -1) {
-    fatal("Font engine failed to initialize.\n");
-  }
-
-  // load fonts
-  font = TTF_OpenFont(get_full_path("resources/fonts/TerminusTTF-Bold-4.39.ttf"), 36);
-  if (font == NULL) {
-    fatal("Error loading font");
-  }
-  gamepanel = new GamePanel(hero, renderer.Get(), font);
-  renderer.SetDrawColor(0xFF, 0xFF, 0xFF, 0xFF);
 }
 
 Game::~Game() {
-  TTF_CloseFont(font);
-  font = NULL;
+  delete font;
 
   renderer = NULL;
   delete window;
@@ -75,53 +80,62 @@ Game::~Game() {
 
 void Game::GameLoop() {
   while (!quit) {
-    renderer->Clear();
-    renderer->SetViewport(SDL2pp::Rect(MAIN_VIEWPORT_RECT));
-    auto bgTexture2 = SDL2pp::Texture(bgTexture.get());
-    renderer->Copy(bgTexture2, SDL2pp::NullOpt, SDL2pp::NullOpt);
-    while (SDL_PollEvent(&e) != 0) {
-      SystemLoop(&e, &quit);
-    }
-    const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
-    hero->loop(currentKeyStates);
-    auto heroSheet = SDL2pp::Texture(hero->spriteSheet.get());
-    renderer->Copy(heroSheet,
-        SDL2pp::Rect(hero->HeroState[hero->state]), SDL2pp::Rect(hero->position));
-
-    hero->boomerangs.erase(
-        std::remove_if(hero->boomerangs.begin(), hero->boomerangs.end(),
-          [](Boomerang* i) { return i->outOfBounds(); }),
-        hero->boomerangs.end());
-    for (auto& boomerang : hero->boomerangs) {
-      boomerang->loop();
-      if (!boomerang->outOfBounds()) {
-        boomerang->draw();
+    try {
+      renderer->SetViewport(SDL2pp::Rect(MAIN_VIEWPORT_RECT));
+      auto bgTexture2 = SDL2pp::Texture(bgTexture.get());
+      renderer->Copy(bgTexture2, SDL2pp::NullOpt, SDL2pp::NullOpt);
+      while (SDL_PollEvent(&e) != 0) {
+        SystemLoop(&e, &quit);
       }
-      SDL_assert(renderer->Get() == hero->renderer);
-      SDL_assert(renderer->Get() == boomerang->renderer);
-    }
-    gamepanel->DrawStats();
+      const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
+      hero->loop(currentKeyStates);
+      auto heroSheet = SDL2pp::Texture(hero->spriteSheet.get());
+      renderer->Copy(heroSheet, SDL2pp::Rect(hero->HeroState[hero->state]),
+                     SDL2pp::Rect(hero->position));
 
-    renderer->Present();
-    SDL_Delay(16);
+      hero->boomerangs.erase(
+          std::remove_if(hero->boomerangs.begin(), hero->boomerangs.end(),
+                         [](Boomerang* i) { return i->outOfBounds(); }),
+          hero->boomerangs.end());
+      for (auto& boomerang : hero->boomerangs) {
+        boomerang->loop();
+        if (!boomerang->outOfBounds()) {
+          boomerang->draw();
+        }
+        SDL_assert(renderer->Get() == hero->renderer);
+        SDL_assert(renderer->Get() == boomerang->renderer);
+      }
+      gamepanel->DrawStats();
+
+      renderer->Present();
+      SDL_Delay(16);
+    } catch (SDL2pp::Exception& e) {
+      // Exception stores SDL_GetError() result and name of function which
+      // failed
+      std::cerr << "Error in: " << e.GetSDLFunction() << std::endl;
+      std::cerr << "  Reason: " << e.GetSDLError() << std::endl;
+    } catch (std::exception& e) {
+      // This also works (e.g. "SDL_Init failed: No available video device")
+      std::cerr << e.what() << std::endl;
+    }
   }
 }
 
 GamePanel::GamePanel(Hero* hero, SDL_Renderer* renderer, TTF_Font* font)
-  : hero(hero), renderer(renderer), font(font) {}
+    : hero(hero), renderer(renderer), font(font) {}
 
-  void GamePanel::DrawStats() {
-    char herotext[32];
-    SDL_RenderSetViewport(renderer, &BOTTOM_VIEWPORT_RECT);
+void GamePanel::DrawStats() {
+  char herotext[32];
+  SDL_RenderSetViewport(renderer, &BOTTOM_VIEWPORT_RECT);
 
-    snprintf(herotext, sizeof(herotext), "health %d / %d", hero->stats.current_hp,
-        hero->stats.hp);
-    draw_text(herotext, 0, 0, font, renderer);
-    SDL_RenderSetViewport(renderer, &MAIN_VIEWPORT_RECT);
-  }
+  snprintf(herotext, sizeof(herotext), "health %d / %d", hero->stats.current_hp,
+           hero->stats.hp);
+  draw_text(herotext, 0, 0, font, renderer);
+  SDL_RenderSetViewport(renderer, &MAIN_VIEWPORT_RECT);
+}
 
 void Game::SystemLoop(const SDL_Event* e, bool* quit) {
-  switch(e->type) {
+  switch (e->type) {
     case SDL_QUIT:
       *quit = true;
       break;
@@ -150,19 +164,16 @@ void Game::SystemLoop(const SDL_Event* e, bool* quit) {
           SDL_Log("Window %d exposed", e->window.windowID);
           break;
         case SDL_WINDOWEVENT_MOVED:
-          SDL_Log("Window %d moved to %d,%d",
-              e->window.windowID, e->window.data1,
-              e->window.data2);
+          SDL_Log("Window %d moved to %d,%d", e->window.windowID,
+                  e->window.data1, e->window.data2);
           break;
         case SDL_WINDOWEVENT_RESIZED:
-          SDL_Log("Window %d resized to %dx%d",
-              e->window.windowID, e->window.data1,
-              e->window.data2);
+          SDL_Log("Window %d resized to %dx%d", e->window.windowID,
+                  e->window.data1, e->window.data2);
           break;
         case SDL_WINDOWEVENT_SIZE_CHANGED:
-          SDL_Log("Window %d size changed to %dx%d",
-              e->window.windowID, e->window.data1,
-              e->window.data2);
+          SDL_Log("Window %d size changed to %dx%d", e->window.windowID,
+                  e->window.data1, e->window.data2);
           break;
         case SDL_WINDOWEVENT_MINIMIZED:
           SDL_Log("Window %d minimized", e->window.windowID);
@@ -174,27 +185,24 @@ void Game::SystemLoop(const SDL_Event* e, bool* quit) {
           SDL_Log("Window %d restored", e->window.windowID);
           break;
         case SDL_WINDOWEVENT_ENTER:
-          SDL_Log("Mouse entered window %d",
-              e->window.windowID);
+          SDL_Log("Mouse entered window %d", e->window.windowID);
           break;
         case SDL_WINDOWEVENT_LEAVE:
           SDL_Log("Mouse left window %d", e->window.windowID);
           break;
         case SDL_WINDOWEVENT_FOCUS_GAINED:
-          SDL_Log("Window %d gained keyboard focus",
-              e->window.windowID);
+          SDL_Log("Window %d gained keyboard focus", e->window.windowID);
           break;
         case SDL_WINDOWEVENT_FOCUS_LOST:
-          SDL_Log("Window %d lost keyboard focus",
-              e->window.windowID);
+          SDL_Log("Window %d lost keyboard focus", e->window.windowID);
           break;
         case SDL_WINDOWEVENT_CLOSE:
           SDL_Log("Window %d closed", e->window.windowID);
           break;
         default:
-          SDL_Log("Window %d got unknown event %d",
-              e->window.windowID, e->window.event);
-          break; 
+          SDL_Log("Window %d got unknown event %d", e->window.windowID,
+                  e->window.event);
+          break;
       }
       break;
   }
