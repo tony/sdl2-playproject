6f7f59e57d3164129ba3d0470036466f0391bcd1
Tony Narlock <tony@git-pull.com>
2016-08-08 12:24:19 -0500

    Turn Stage abstract class into pure interface

--- src/stage.cpp
+++ src/stage.cpp
@@ -1,16 +1,9 @@
 #include "stage.h"
 
-Stage::Stage(SDL2pp::Renderer& renderer, spd::logger& console)
-    : renderer(renderer) {
-  console.info("Stage started.");
-}
-
 LevelStage::LevelStage(SDL2pp::Renderer& renderer,
                        std::shared_ptr<ResourceManager> resource_manager,
-                       const std::shared_ptr<StatService>& stat_service,
-                       spd::logger& console)
-    : Stage(renderer, console),
-      bg_texture(resource_manager->GetTexture("bg1")) {
+                       const std::shared_ptr<StatService>& stat_service)
+    : renderer(renderer), bg_texture(resource_manager->GetTexture("bg1")) {
   game_panel =
       std::make_shared<GamePanel>(stat_service, renderer, resource_manager);
   ship = std::make_shared<Ship>(renderer, resource_manager);
--- src/stage.h
+++ src/stage.h
@@ -9,26 +9,22 @@ namespace spd = spdlog;
 
 class Stage {
  public:
-  Stage(SDL2pp::Renderer& renderer, spd::logger& console);
   virtual void HandleInput(const Uint8* currentKeyStates) = 0;
   virtual void Update() = 0;
-
- protected:
-  SDL2pp::Renderer& renderer;
 };
 
 class LevelStage : Stage {
  public:
   LevelStage(SDL2pp::Renderer& renderer,
              std::shared_ptr<ResourceManager> resource_manager,
-             const std::shared_ptr<StatService>& stat_service,
-             spd::logger& console);
+             const std::shared_ptr<StatService>& stat_service);
   void HandleInput(const Uint8* currentKeyStates) override final;
   void Update() override final;
 
  private:
+  SDL2pp::Renderer& renderer;
+  const std::shared_ptr<SDL2pp::Texture>& bg_texture;
   std::shared_ptr<Ship> ship;
   std::shared_ptr<ResourceManager> resource_manager;
-  const std::shared_ptr<SDL2pp::Texture>& bg_texture;
   std::shared_ptr<GamePanel> game_panel = nullptr;
 };
