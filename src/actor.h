5980d75182fab8d3a9bf225ed21e9a93a96c4c5c
Tony Narlock <tony@git-pull.com>
2016-08-08 07:23:05 -0500

    optional subscribe_rect

--- src/actor.h
+++ src/actor.h
@@ -26,6 +26,7 @@ class Actor {
   const std::shared_ptr<ResourceManager>& resource_manager;
   SDL2pp::Rect position;
   SDL2pp::Point velocity;
+  SDL2pp::Optional<SDL2pp::Rect> subsprite_rect;
   SDL2pp::Texture sprite = nullptr;
   SDL2pp::Texture shadow = nullptr;
 };
